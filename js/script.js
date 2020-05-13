25e5e00c54336703aaba798a637276640389c43c
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-05-13 13:57:23 -0400

    edit style

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ function editMarkdown() {
       
       target.innerHTML = html;
   }
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     document.getElementById("header").style.fontSize = "30px";
+//     document.getElementById("img").style.width = "200px";
+//   } else {
+//     document.getElementById("header").style.fontSize = "60px";
+//     document.getElementById("img").style.width = "400px";
+//   }
+// }
