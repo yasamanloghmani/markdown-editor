972df2e6da4e2da27c77c7e24704d33cfb56591e
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-05-13 15:22:02 -0400

    Add theme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+const bodyFont = getComputedStyle(document.body).getPropertyValue('--font');
+const bgColor = getComputedStyle(document.body).getPropertyValue('--bg-color');
+const textColor = getComputedStyle(document.body).getPropertyValue('--text-c olor');
+
+const fontSelect = document.querySelector('.font-selector');
+const textColorPicker = document.querySelector('.text-color-pricker');
+const bgtColorPicker = document.querySelector('.bg-color-pricker');
+
+const theme= [fontSelect, textColorPicker, bgtColorPicker ];
+theme.forEach(function(option){
+    option.addEventListener('change', (evt) => {
+
+        document.body.style.setProperty(option.dataset.themeValue, evt.target.value)
+    });
+});
+
+
 function editMarkdown() {
     var text = document.getElementById('markdownDiv').value,
         target = document.getElementById('resultDiv'),
